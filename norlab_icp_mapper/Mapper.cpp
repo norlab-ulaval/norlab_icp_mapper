4509272e945bda53f3300941c15a7a70021bc9e8
simonpierredeschenes <simonpierredeschenes@gmail.com>
2022-06-01 20:12:46 -0400

    Switched to GaussianToGaussianToPlane.

--- norlab_icp_mapper/Mapper.cpp
+++ norlab_icp_mapper/Mapper.cpp
@@ -63,7 +63,7 @@ norlab_icp_mapper::Mapper::Mapper(std::string icpConfigFilePath, std::string inp
 	{
 		PM::Parameters errorMinimizerParams;
 		errorMinimizerParams["scaleFactor"] = std::to_string(scaleFactor);
-		std::shared_ptr<PM::ErrorMinimizer> errorMinimizer = PM::get().ErrorMinimizerRegistrar.create("GaussianToGaussianErrorMinimizer", errorMinimizerParams);
+		std::shared_ptr<PM::ErrorMinimizer> errorMinimizer = PM::get().ErrorMinimizerRegistrar.create("GaussianToGaussianToPlaneErrorMinimizer", errorMinimizerParams);
 		icp.errorMinimizer = errorMinimizer;
 	}
 
